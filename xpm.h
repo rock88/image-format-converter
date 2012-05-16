77f6894a69b68ac7188c9ae1bf14309064e14cb2
Naskel@gmail.com <Naskel@gmail.com@10623526-9321-6f5d-2cf8-dcd98e5121c1>
2012-05-16 13:11:35 +0000

    xpm loader header

---
+++ xpm.h
@@ -0,0 +1,41 @@
+/* 
+ * xpm.h
+ *
+ * Copyright 2011-2012 ESTEVE Olivier <naskel .[.at.]. gmail.com>
+ *
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this software; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ *
+ * $Log: xpm.h,v $
+ *
+ *
+ *
+ *
+ *
+ *
+ * 
+ * 
+ */
+#ifndef __IMAGE_CONVERTER_XPM_INCLUDE_H_
+#define __IMAGE_CONVERTER_XPM_INCLUDE_H_
+
+uchar* load_xpm( const char *name, int &width, int &height );
+
+#endif	// __IMAGE_CONVERTER_XPM_INCLUDE_H_
+
+// -----------------------------------------------------------------------------
+// xpm.h - Last Change: $Date: 2012-05-15 23:36:52 $ - End Of File
+// -----------------------------------------------------------------------------
